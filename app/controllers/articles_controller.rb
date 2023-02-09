97b0da3efa7f4251ae5e67b0a46e776cd66ec5b1
Vitor Setem <vitorsetem@gmail.com>
2023-02-08 22:06:07 -0300

    Create update on articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,13 +9,31 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
-    # redirect_to article_path(@article) <-Isto faz o mesmo que a linha de cima
+    if @article.save
+      flash[:notice] = "Article was created successfully"
+      redirect_to @article 
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article updated"
+      redirect_to @article
+    else 
+      render 'edit'
+    end 
+  end 
+
 end 
