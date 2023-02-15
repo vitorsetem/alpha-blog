137f35bde0587eba6685745e9dedca611e69521a
Vitor Setem <vitorsetem@gmail.com>
2023-02-15 15:03:30 -0300

    Some DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only: [:show, :edit, :update, :destroy] #roda o set_article antes do que está abaixo
+  
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,11 +14,10 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully"
       redirect_to @article 
@@ -27,8 +27,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article updated"
       redirect_to @article
     else 
@@ -37,9 +36,18 @@ class ArticlesController < ApplicationController
   end 
 
   def destroy 
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path #articles_path manda para o caminho dos articles
   end 
+
+  private #private é que o que está abaixo é só para este controller, não deve ter um end no final dele
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end 
